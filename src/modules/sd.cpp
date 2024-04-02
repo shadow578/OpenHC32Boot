5c2436fce706b4163fc76d2326a9373bed37982a
Chris <52449218+shadow578@users.noreply.github.com>
2024-04-02 13:24:18 +0000

    sd file open / close routines

---
+++ src/modules/sd.cpp
@@ -0,0 +1,61 @@
+#include "sd.h"
+#include "../config.h"
+
+namespace sd 
+{
+  /**
+   * @brief FatFS file system object
+   */
+  FATFS fs;
+
+  bool get_update_file(FIL &file, const char *path)
+  {
+    // mount the file system
+    FRESULT res = f_mount(&fs, "", /* mount now */ 1);
+    if (res != FR_OK)
+    {
+      printf("f_mount() failed: %d\n", res);
+      return false;
+    }
+
+    // try to open the file
+    res = f_open(&file, path, FA_READ);
+    if (res != FR_OK)
+    {
+      printf("f_open() failed: %d\n", res);
+      return false;
+    }
+
+    // assume everything is ok
+    // if the file is more than 0 bytes
+    return f_size(&file) > 0;
+  }
+
+  void close_update_file(FIL &file, const char *path)
+  {
+    // close the file
+    FRESULT res = f_close(&file);
+    if (res != FR_OK)
+    {
+      printf("f_close() failed: %d\n", res);
+      // don't care for error here
+    }
+
+    #if DELETE_FIRMEWARE_UPDATE_FILE == 1
+      res = f_unlink(path);
+      if (res != FR_OK)
+      {
+        printf("f_unlink() failed: %d\n", res);
+        // don't care for error here
+      }
+    #endif
+
+    // unmount the file system
+    res = f_unmount("");
+    if (res != FR_OK)
+    {
+      printf("f_unmount() failed: %d\n", res);
+      // don't care for error here
+    }
+  }
+} // namespace sd
--- src/modules/sd.h
+++ src/modules/sd.h
@@ -1,3 +1,26 @@
 #pragma once
 #include "sd/fatfs/ff.h"
 #include "sd/sdio.h"
+
+namespace sd 
+{
+  /**
+   * @brief open the update file at the given path
+   * @param file file object
+   * @param path path to the file
+   * @return true if the file was opened successfully
+   * 
+   * @note assumes only one file is opened at a time
+   */
+  bool get_update_file(FIL &file, const char *path);
+
+  /**
+   * @brief close the update file, deleting it if requested
+   * @param file file object
+   * @param path path to the file (needed for deletion)
+   * @return true if the file was closed successfully
+   *
+   * @note assumes only one file is opened at a time
+   */
+  void close_update_file(FIL &file, const char *path);
+} // namespace sd
