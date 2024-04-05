53acdd2cdd45ea96b2fc7ab8cdbb56f407a877e3
Chris <52449218+shadow578@users.noreply.github.com>
2024-04-05 12:10:50 +0000

    (maybe) fix SHA256 calculation

--- src/modules/hash/sha256.cpp
+++ src/modules/hash/sha256.cpp
@@ -14,28 +14,34 @@
   namespace hash
   {
     /**
+     * @brief the total length of the data pushed to the hash peripheral using push_data()
+     */
+    static uint32_t total_length = 0;
+
+    /**
      * @brief is this the first push in the current session? 
      */
-    static bool is_first_push = true;
+    #define is_first_push (total_length == 0)
 
     /**
      * @brief wait until the hash peripheral is ready again
      */
     void wait_for_ready()
     {
-      while(M4_HASH->CR_f.START != 0) { /* nada */ }
+      while(bM4_HASH_CR_START != 0) { /* nada */ }
     }
 
     /**
      * @brief push a 512-bit block to the hash peripheral, adding padding as needed
      * @param block the block to push.
      * @param len the length of the block
-     * @param scratch a scratch buffer to use. exactly 64 bytes long.
      * 
      * @note expects len to be 64 bytes except for the last block
+     * @note automatically starts the hash calculation
      */
-    void push_block(const uint8_t *block, const uint32_t len, uint8_t *scratch)
+    void push_block(const uint8_t *block, const uint32_t len)
     {
+      uint8_t scratch[HASH_GROUP_LEN];
       std::fill(scratch, scratch + HASH_GROUP_LEN, 0);
 
       // copy data to buffer
@@ -66,45 +72,23 @@
       
 
       // start hash calculation
-      M4_HASH->CR_f = {
-        .START = 1,
-        .FST_GRP = is_first_push ? 1ul : 0ul, // first group?
-      };
+      bM4_HASH_CR_FST_GRP = is_first_push ? 1ul : 0ul; // first group?
+      bM4_HASH_CR_START = 1;
     }
 
-    bool start()
-    {
-      // enable peripheral clock
-      PWC_Fcg0PeriphClockCmd(PWC_FCG0_PERIPH_HASH, Enable);
-
-      // stop any ongoing hash calculation
-      M4_HASH->CR = 0;
-      is_first_push = true;
-      return true;
-    }
-
-    bool push_data(const uint8_t *data, const uint32_t len)
+    /**
+     * @brief write the length of the data to the hash peripheral
+     * @param len the length to write
+     */
+    void write_len(const uint32_t len)
     {
       uint8_t scratch[HASH_GROUP_LEN];
-
-      // wait for hash calculation to finish
-      wait_for_ready();
-
-      // write data to hash peripheral
-      uint32_t remaining_bytes = len;
-      while(remaining_bytes > 0)
-      {
-        uint32_t push_len = minimum(remaining_bytes, HASH_GROUP_LEN);
-        push_block(data, push_len, scratch);
-
-        data += HASH_GROUP_LEN;
-        remaining_bytes -= push_len;
-      }
+      std::fill(scratch, scratch + HASH_GROUP_LEN, 0);
 
       // write length to hash peripheral
       uint32_t len_lo = (len >> 29u) & 0x7u;
       uint32_t len_hi = (len << 3u);
-      std::fill(scratch, scratch + HASH_GROUP_LEN, 0);
+
       scratch[63] = static_cast<uint8_t>(len_lo);
       scratch[62] = static_cast<uint8_t>(len_lo >> 8);
       scratch[61] = static_cast<uint8_t>(len_lo >> 16);
@@ -113,18 +97,43 @@
       scratch[58] = static_cast<uint8_t>(len_hi >> 8);
       scratch[57] = static_cast<uint8_t>(len_hi >> 16);
       scratch[56] = static_cast<uint8_t>(len_hi >> 24);
-      push_block(scratch, HASH_GROUP_LEN, scratch);
 
-      // start hash calculation
-      M4_HASH->CR_f = {
-        .START = 1,
-        .FST_GRP = is_first_push ? 1ul : 0ul, // first group?
-      };
+      push_block(scratch, HASH_GROUP_LEN);
+    }
+
+    bool start()
+    {
+      // enable peripheral clock
+      PWC_Fcg0PeriphClockCmd(PWC_FCG0_PERIPH_HASH, Enable);
+
+      // stop any ongoing hash calculation
+      bM4_HASH_CR_START = 0;
+      total_length = 0;
+      return true;
+    }
+
+    bool push_data(const uint8_t *data, const uint32_t len)
+    {
+      // write data to hash peripheral
+      uint32_t remaining_bytes = len;
+      while(remaining_bytes > 0)
+      {
+        const uint32_t push_len = minimum(remaining_bytes, HASH_GROUP_LEN);
+        push_block(data, push_len);
+
+        data += push_len;
+        remaining_bytes -= push_len;
+      }
+
+      total_length += len;
       return true;
     }
 
     bool get_hash(hash_t &hash)
     {
+      // write total length to hash peripheral
+      write_len(total_length);
+
       // wait for hash calculation to finish
       wait_for_ready();
 
@@ -135,6 +144,10 @@
       {
         hash_out[i] = *hash_hr++;
       }
+
+      // de-init
+      bM4_HASH_CR_START = 0;
+      M4_HASH->CR = 0;
 
       return true;
     }
