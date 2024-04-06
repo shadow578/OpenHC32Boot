51a3b3e19ce8621b6a254dd150393aa5e02129ad
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-04-06 09:35:05 +0200

    update sysclock comment according to ref manual

--- src/modules/sysclock.cpp
+++ src/modules/sysclock.cpp
@@ -6,8 +6,13 @@ namespace sysclock
 { 
   void _apply(const bool restore)
   {
-    // SDIO seemingly requires HCLK and EXCLK to be 2:1
-    // so set up the clock divider accordingly
+    // when setting the clock configuration: 
+    // setup EXCLK clock according to HC32F460 Reference Manual, Section 4.4 "Working Clock Specification", Note 2:
+    // "HCLK frequency: EXCLK frequency = 2:1, 4:1, 8:1, 16:1, 32:1"
+    // PCLKx are also set up according to the same section, with 1:1 ratio.
+    //
+    // when restoring the clock configuration:
+    // set all clocks to 1:1 ratio, according to Section 4.11.21 "CMU Clock Divider Configuration Register (CMU_SCFGR)"
     stc_clk_sysclk_cfg_t clock_divider_config = {
         .enHclkDiv = ClkSysclkDiv1,                             // HCLK  = 8 MHz (CPU)
         .enExclkDiv = restore ? ClkSysclkDiv1 : ClkSysclkDiv2,  // EXCLK = 8 / 4 MHz (SDIO)
