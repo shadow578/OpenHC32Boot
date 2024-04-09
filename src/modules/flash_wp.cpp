9cba99a801a0eea8ac0b4e390e8a120ee978bbb2
shadow578 <52449218+shadow578@users.noreply.github.com>
2024-04-09 17:41:08 +0200

    fix flash_wp module

--- src/modules/flash_wp.cpp
+++ src/modules/flash_wp.cpp
@@ -5,21 +5,26 @@
 
 namespace flash::write_protect
 {
+  void set_protection_window(const uint32_t window_start, const uint32_t window_end)
+  {
+    EFM_Unlock();
+    M4_EFM->FPMTSW_f.FPMTSW = window_start;
+    M4_EFM->FPMTEW_f.FPMTEW = window_end;
+    EFM_Lock();
+  }
+
   void enable()
   {
     // according to the reference manual, section 7.6.7 "FLASH Wipe, Programming Window Protection"
     // the protection window defines the area of flash that is NOT protected, e.g. the area that can be erased or written to
     constexpr uint32_t start_address = (APP_BASE_ADDRESS - 1) & 0x000FFFFF;
     constexpr uint32_t end_address = (get_flash_size() - 1) & 0x000FFFFF;
-
-    M4_EFM->FPMTSW_f.FPMTSW = start_address;
-    M4_EFM->FPMTEW_f.FPMTEW = end_address;
+    set_protection_window(start_address, end_address);
   }
 
   void disable()
   {
     // if window start address == window end address, the write protection is disabled
-    M4_EFM->FPMTSW = 0x0;
-    M4_EFM->FPMTEW = 0x0;
+    set_protection_window(0, 0);
   }
 } // namespace flash::write_protect
