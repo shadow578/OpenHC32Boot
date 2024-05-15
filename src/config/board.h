8e4e804146dec97923cfb9622e22f099c72ce187
Chris <52449218+shadow578@users.noreply.github.com>
2024-05-15 10:46:51 +0200

    add creality_e2p24s4 board (#2)

--- src/config/board.h
+++ src/config/board.h
@@ -3,6 +3,8 @@
 
 #if IS_BOARD(VOXELAB_V101)
   #include "boards/voxelab_v101.h"
+#elif IS_BOARD(CREALITY_E2P24S4)
+  #include "boards/creality_e2p24s4.h"
 #else
   #error "Unknown board"
 #endif
--- src/config/boards.h
+++ src/config/boards.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define BOARD_VOXELAB_V101 1000
-
+#define BOARD_CREALITY_E2P24S4 2000
 
 #define IS_BOARD(board) (BOARD == BOARD_##board)
